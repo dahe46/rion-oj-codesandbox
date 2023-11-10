f72ddf2c3a01547fa40c55d3111cd26a90e8c8b0
Rion <2926217401@qq.com>
2023-11-10 10:58:45 +0800

    新增常量

---
+++ src/main/java/com/rionoj/codesandbox/constant/DockerConstant.java
@@ -0,0 +1,10 @@
+package com.rionoj.codesandbox.constant;
+
+/**
+ * @author Rion
+ */
+public class DockerConstant {
+
+    public static final String JDK_IMAGE = "openjdk:8-alpine";
+    public static final String CONTAINER_NAME = "rion-openjdk";
+}
---
+++ src/main/java/com/rionoj/codesandbox/constant/UserCodeConstant.java
@@ -0,0 +1,13 @@
+package com.rionoj.codesandbox.constant;
+
+/**
+ * 用户代码常量
+ *
+ * @author Rion
+ * @date 2023/09/10
+ */
+public class UserCodeConstant {
+
+    public static final String TMP_CODE_DIR_NAME = "tmpCode";
+
+}
